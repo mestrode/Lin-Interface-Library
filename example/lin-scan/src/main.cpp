b106e2918547ba48579e9d870e4bab3ea7ed9ca7
mestrode <git@mestro.de>
2024-09-29 20:59:39 +0200

    update scan example for 1.0.0

--- example/lin-scan/src/main.cpp
+++ example/lin-scan/src/main.cpp
@@ -1,23 +1,26 @@
 #include <Arduino.h>
 #include <LinFrameTransfer.hpp>
 
-// using UART 1 for LinBus
-LinFrameTransfer LinBus(1);
+// using UART 2 for LinBus, UART1 for debug log
+LinFrameTransfer LinBus(2, Serial1);
 
 int LIN_ScanIDs()
 {
     int Result = 0;
     Serial.print("\n\n############# LIN ID scan started\n");
+    Serial.print("FIDs confirmed: ");
     // Scan all IDs
     for (int i = 0; i <= 0x3F; i++)
     {
         if (LinBus.readFrame(i))
         {
             Result++;
+            Serial.print(i);
+            Serial.print(", ");
         }
         delay(10);
     }
-    Serial.print("############# Chk Sum valid on ");
+    Serial.print("\n############# Chk Sum valid on ");
     Serial.print(Result);
     Serial.print(" Frames\n############# LIN ID scan finished\n\n");
 
