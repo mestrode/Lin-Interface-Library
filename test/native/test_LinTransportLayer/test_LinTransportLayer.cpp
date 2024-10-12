026d1d6f932b0e98e22e2bf28ebb86f2a2f38287
mestrode <git@mestro.de>
2024-10-12 13:57:10 +0200

    increase robustness of code

--- test/native/test_LinTransportLayer/test_LinTransportLayer.cpp
+++ test/native/test_LinTransportLayer/test_LinTransportLayer.cpp
@@ -28,7 +28,7 @@ void test_write_DTL_MasterRequest_SF_SF()
     std::cout << "\n\n\nRunning test: " << __FUNCTION__ << std::endl;
 
     // Master Request
-    uint8_t NAD_value = 0x0A;
+    constexpr uint8_t NAD_value = 0x0A;
     uint8_t NAD = NAD_value;
     struct Request
     {
@@ -41,7 +41,7 @@ void test_write_DTL_MasterRequest_SF_SF()
     // Slave Response
     struct Response {
         std::vector<uint8_t> head {
-            0x0A, // NAD
+            NAD_value, // NAD
             0x06  // Single Frame, 6 Bytes
         };
         std::vector<uint8_t> payload {
@@ -60,7 +60,7 @@ void test_write_DTL_MasterRequest_SF_SF()
     auto result = linTransportLayer->writePDU(NAD, request.payload);
 
     TEST_ASSERT_TRUE(result.has_value());
-    TEST_ASSERT_EQUAL(0x0A, NAD_value);
+    TEST_ASSERT_EQUAL(NAD_value, NAD_value);
 
     TEST_ASSERT_EQUAL(response.payload.size(), result.value().size());
     TEST_ASSERT_EQUAL_MEMORY(response.payload.data(), result.value().data(), response.payload.size());
@@ -69,7 +69,7 @@ void test_write_DTL_MasterRequest_SF_SF()
     std::vector<uint8_t> expectation = {
     // Master Request
         0x00, 0x55, 0x3C, // Frame Head = Master Request
-        0x0A, // NAD
+        NAD_value, // NAD
         0x03, // Single Frame, 3 Bytes
         0x22, 0x06, 0x2E, // payload
         0xff, 0xff, 0xff, // filing bytes
@@ -90,16 +90,17 @@ void test_write_DTL_MasterRequest_SF_SF2()
     std::cout << "\n\n\nRunning test: " << __FUNCTION__ << std::endl;
 
     // Master Request
-    uint8_t NAD_value = 0x0A;
-    uint8_t NAD = NAD_value;
+    constexpr uint8_t NAD_request = 0x0A;
+    uint8_t NAD = NAD_request;
     std::vector<uint8_t> payload = {
         0x22, // SID
         0x06, 0x2E, 0x87, 0x47, 0x41 // real payload
     };
 
     // Slave Response
+    constexpr uint8_t NAD_slave = NAD_request;
     std::vector<uint8_t> mock_DTL_response_head {
-        0x0A, // NAD
+        NAD_slave, // NAD
         0x05  // Type | LEN
     }; // Single Frame, 6 Bytes
     std::vector<uint8_t> mock_DTL_response_payload {
@@ -119,7 +120,7 @@ void test_write_DTL_MasterRequest_SF_SF2()
     auto result = linTransportLayer->writePDU(NAD, payload);
 
     TEST_ASSERT_TRUE(result.has_value());
-    TEST_ASSERT_EQUAL(0x0A, NAD_value);
+    TEST_ASSERT_EQUAL(NAD_slave, NAD);
 
     TEST_ASSERT_EQUAL(mock_DTL_response_payload.size(), result.value().size());
     TEST_ASSERT_EQUAL_MEMORY(mock_DTL_response_payload.data(), result.value().data(), mock_DTL_response_payload.size());
@@ -128,9 +129,9 @@ void test_write_DTL_MasterRequest_SF_SF2()
     std::vector<uint8_t> expectation = {
     // Master Request
         0x00, 0x55, 0x3C, // Frame Head = Master Request
-        0x0A, // NAD
+        NAD_request, // NAD
         0x06, // Single Frame, 3 Bytes
-        0x22, 0x06, 0x2E, 0x87, 0x47, 'A', // payload
+        0x22, 0x06, 0x2E, 0x87, 0x47, 0x41, // payload
         // no filing bytes
         137, // Frame Checksum
 
@@ -147,32 +148,33 @@ void test_write_DTL_MasterRequest_SF_MultiFrame()
     std::cout << "\n\n\nRunning test: " << __FUNCTION__ << std::endl;
 
     // Master Request
-    uint8_t NAD_value = 0x7F; // Wildcard
-    uint8_t NAD = NAD_value;
+    constexpr uint8_t NAD_request = 0x7F; // Wildcard
+    uint8_t NAD = NAD_request;
     std::vector<uint8_t> payload = {
         0x22, // SID
         0x06, 0x5E // real payload
     };
 
     // Slave Response
+    constexpr uint8_t NAD_slave = 0x0A;
     std::vector<uint8_t> mock_DTL_response {
     // FF
-        0x0A, // NAD
+        NAD_slave, // NAD
         0x10, 0x14,  // Type | LEN
         0x62, 0x06, 0x5E, 0x96, 0x54, // payload
         0x20, // Frame Checksum
     // 1st CC
-        0x0A, // NAD
+        NAD_slave, // NAD
         0x21,  // Type | SN
         0x62, 0x06, 0x5E, 0x44, 0x55, 0x78, // payload
         0xFB, // Frame Checksum
     // 2nd CC
-        0x0A, // NAD
+        NAD_slave, // NAD
         0x22,  // Type | SN
         0x54, 0x10, 0x01, 0x00, 0xFF, 0xEE, // payload
         0x7F, // Frame Checksum
     // 3rd CC
-        0x0A, // NAD
+        NAD_slave, // NAD
         0x23,  // Type | SN
         0x12, 0x99, 0x21, // payload
         0xFF, 0xFF, 0xFF, // fill bytes
@@ -183,14 +185,14 @@ void test_write_DTL_MasterRequest_SF_MultiFrame()
     auto result = linTransportLayer->writePDU(NAD, payload);
 
     std::vector<uint8_t> mock_payload {
-        0x62, 0x06, 0x5E, 0x96, 0x54, // payload
-        0x62, 0x06, 0x5E, 0x44, 0x55, 0x78, // payload
-        0x54, 0x10, 0x01, 0x00, 0xFF, 0xEE, // payload
-        0x12, 0x99, 0x21, // payload
+        0x62, 0x06, 0x5E, 0x96, 0x54,
+        0x62, 0x06, 0x5E, 0x44, 0x55, 0x78,
+        0x54, 0x10, 0x01, 0x00, 0xFF, 0xEE,
+        0x12, 0x99, 0x21
     };
 
     TEST_ASSERT_TRUE(result.has_value());
-    TEST_ASSERT_EQUAL(0x0A, NAD);
+    TEST_ASSERT_EQUAL(NAD_slave, NAD);
 
     TEST_ASSERT_EQUAL(mock_payload.size(), result.value().size());
     TEST_ASSERT_EQUAL_MEMORY(mock_payload.data(), result.value().data(), mock_payload.size());
@@ -199,7 +201,7 @@ void test_write_DTL_MasterRequest_SF_MultiFrame()
     std::vector<uint8_t> expectation = {
     // Master Request
         0x00, 0x55, 0x3C, // Frame Head = Master Request
-        0x7F, // NAD
+        NAD_request, // NAD
         0x03, // Single Frame, 3 Bytes
         0x22, 0x06, 0x5E, // payload
         0xFF, 0xFF, 0xFF, // filing bytes
@@ -226,17 +228,18 @@ void test_write_DTL_MasterRequest_MultiFrame_SF()
     std::cout << "fillbytes: yes + yes" << std::endl;
 
     // Master Request
-    uint8_t NAD_value = 0x7F; // Wildcard
-    uint8_t NAD = NAD_value;
+    constexpr uint8_t NAD_request = 0x7F; // Wildcard
+    uint8_t NAD = NAD_request;
     std::vector<uint8_t> payload = {
-        0x62, 0x06, 0x5E, '3', 'C',
-        '8', '9', '5', '9', '5', '3',
-        '7', ' ', ' '
+        0x62, 0x06, 0x5E, 0x33, 0x43,
+        0x38, 0x39, 0x35, 0x39, 0x35, 0x33,
+        0x37, 0x20, 0x20
     };
 
     // Slave Response
+    constexpr uint8_t NAD_slave = 0x0A;
     std::vector<uint8_t> mock_DTL_response_head {
-        0x0A, // NAD
+        NAD_slave, // NAD
         0x01  // Single Frame, 1 Bytes
     }; 
     std::vector<uint8_t> mock_DTL_response_payload {
@@ -260,7 +263,7 @@ void test_write_DTL_MasterRequest_MultiFrame_SF()
     };
 
     TEST_ASSERT_TRUE(result.has_value());
-    TEST_ASSERT_EQUAL(0x0A, NAD);
+    TEST_ASSERT_EQUAL(NAD_slave, NAD);
 
     TEST_ASSERT_EQUAL(mock_DTL_response_payload.size(), result.value().size());
     TEST_ASSERT_EQUAL_MEMORY(mock_DTL_response_payload.data(), result.value().data(), mock_DTL_response_payload.size());
@@ -268,21 +271,21 @@ void test_write_DTL_MasterRequest_MultiFrame_SF()
     std::vector<uint8_t> expectation = {
     // Master Request
         0x00, 0x55, 0x3C, // Frame Head
-        0x7F,
+        NAD_request,
         0x10, 0x0E, // First Frame, over all len = e Bytes
-        0x62, 0x06, 0x5E, '3', 'C', // payload
+        0x62, 0x06, 0x5E, 0x33, 0x43, // payload
         0x25, // Frame Checksum
 
         0x00, 0x55, 0x3C, // Frame Head
-        0x7F,
+        NAD_request,
         0x21, // Consegutive Frame, count = 1
-        '8', '9', '5', '9', '5', '3', // payload
+        0x38, 0x39, 0x35, 0x39, 0x35, 0x33, // payload
         0x17, // Frame Checksum
 
         0x00, 0x55, 0x3C, // Frame Head
-        0x7F,
+        NAD_request,
         0x22, // Consegutive Frame, count = 2
-        '7', ' ', ' ', // payload
+        0x37, 0x20, 0x20, // payload
         0xFF, 0xFF, 0xFF, // fill bytes
         0xe6, // Frame Checksum
 
@@ -301,8 +304,8 @@ void test_write_DTL_MasterRequest_MultiFrame_MultiFrame()
     std::cout << "\n\n\nRunning test: " << __FUNCTION__ << std::endl;
     std::cout << "fillbytes: no + no" << std::endl;
 
-    uint8_t NAD_value = 0x7F; // wildcard
-    uint8_t NAD = NAD_value;
+    constexpr uint8_t NAD_request = 0x7F; // wildcard
+    uint8_t NAD = NAD_request;
 
     // Master Request
     struct Request {
@@ -314,7 +317,7 @@ void test_write_DTL_MasterRequest_MultiFrame_MultiFrame()
     } request;
 
     // Slave Response
-    const uint8_t NAD_slave = 0x0A;
+    constexpr const uint8_t NAD_slave = 0x0A;
     struct Response {
         std::vector<uint8_t> mock_DTLs {
         // FF
@@ -337,9 +340,9 @@ void test_write_DTL_MasterRequest_MultiFrame_MultiFrame()
     } response;
 
     std::vector<uint8_t> payload {
-        0xD9, 0xA2, 0xA3, 0xA4, 0xA5, // payload
-        0xB1, 0xB2, 0xB3, 0xB4, 0xB5, 0xB6, // payload
-        0xC1, 0xC2, 0xC3, 0xC4, 0xC5, 0xC6 // payload
+        0xD9, 0xA2, 0xA3, 0xA4, 0xA5,
+        0xB1, 0xB2, 0xB3, 0xB4, 0xB5, 0xB6,
+        0xC1, 0xC2, 0xC3, 0xC4, 0xC5, 0xC6
     };
 
     linDriver->mock_Input(response.mock_DTLs);
@@ -355,19 +358,19 @@ void test_write_DTL_MasterRequest_MultiFrame_MultiFrame()
     std::vector<uint8_t> expectation = {
     // Master Request
         0x00, 0x55, 0x3C, // Frame Head
-        0x7F, // NAD
+        NAD_request, // NAD
         0x10, 0x11, // First Frame, over all len = 17 Bytes
         0x99, 0x98, 0x97, 0x96, 0x95, // payload
         0x69, // Frame Checksum
 
         0x00, 0x55, 0x3C, // Frame Head
-        0x7F, // NAD
+        NAD_request, // NAD
         0x21, // Consegutive Frame, count = 1
         0x89, 0x88, 0x87, 0x86, 0x85, 0x84, // payload
         0x35, // Frame Checksum
 
         0x00, 0x55, 0x3C, // Frame Head
-        0x7F, // NAD
+        NAD_request, // NAD
         0x22, // Consegutive Frame, count = 2
         0x79, 0x78, 0x77, 0x76, 0x75, 0x74, // payload
         // no fill bytes
